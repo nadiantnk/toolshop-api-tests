d68efba4de2412f048b569100b8836567b808cc2
nadiantnk <wasdass98@gmail.com>
2026-08-10 13:07:17 +0700

    Add login test with token check

--- test_products.py
+++ test_products.py
@@ -8,3 +8,15 @@ def test_get_product_returns_200():
 def test_related_products_not_found_returns_404():
     response = requests.get("https://api-with-bugs.practicesoftwaretesting.com/products/999/related")
     assert response.status_code == 404
+
+def test_login_returns_200():
+    response = requests.post(
+        "https://api-with-bugs.practicesoftwaretesting.com/users/login",
+        json={
+            "email": "customer@practicesoftwaretesting.com",
+            "password": "welcome01"
+        }
+    )
+    assert response.status_code == 200
+    body = response.json()
+    assert "access_token" in body
