01556d63cc725144bd61d120c8ecf780f25fc6f1
nadiantnk <wasdass98@gmail.com>
2026-08-10 13:39:41 +0700

    Add brands and negative product tests

--- test_products.py
+++ test_products.py
@@ -20,3 +20,18 @@ def test_login_returns_200():
     assert response.status_code == 200
     body = response.json()
     assert "access_token" in body
+
+def test_list_brands_200():
+    response = requests.get("https://api-with-bugs.practicesoftwaretesting.com/brands")
+    assert response.status_code == 200
+
+def test_login_with_invalid_password_401():
+    response = requests.post(
+        "https://api-with-bugs.practicesoftwaretesting.com/users/login",
+        json={
+        "email": "customer@practicesoftwaretesting.com",
+        "password": "wrongpassword"
+        }
+    )
+    assert response.status_code == 401
+    
